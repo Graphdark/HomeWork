6a12ab3f5390ac275144c1c443a350f00be6aa70
Graphdark <67462210+Graphdark@users.noreply.github.com>
2021-11-17 17:48:08 +0400

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,32 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+extern int a, b, c;
+
+int main()
+{
+//	int a = 3;
+//	int b = 4;
+//	int c = 35;
+	int d = 48;
+	std::cout << (a * (b + (c / static_cast<float>(d)))) << std::endl;
+	int e;
+	std::cin >> e;
+	if (e <= 21)
+	{
+		std::cout << (21 - e) << std::endl;
+	}
+	else
+	{
+		std::cout << ((e - 21) * 2) << std::endl;
+	}
+	e <= 21 ? std::cout << (21 - e) << std::endl : std::cout << ((e - 21) * 2) << std::endl;
+	int array[3][3][3];
+	array[1][1][1] = 50;
+	int* p;
+	p = &array[1][1][1];
+	std::cout << *p;
+
+
+}
---
+++ Source.cpp
@@ -0,0 +1,3 @@
+int a = 3;
+int b = 8;
+int c = 6;
